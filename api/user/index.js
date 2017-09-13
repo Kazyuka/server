c0b8ad255902730b9a5924332827714b5f42907f
Kazyuka <rkazyuka@mail.ru>
2017-09-13 16:28:36 +0300

    create new request

--- api/user/index.js
+++ api/user/index.js
@@ -6,6 +6,7 @@ var userController = require('./userController');
 var router = express.Router();
 
 router.post("/add_user/", userController.create);
+router.get("/get_user/:id", userController.findUser);
 router.put("/add_new_friend/:id", userController.addFriend);
 router.delete("/remove_friend/:id/", userController.removeFriend);
 router.delete("/remove_user/:id/", userController.removeUser);
