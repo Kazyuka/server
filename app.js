ffa469decff2e2e637c691cefd7f176d54db7cf3
Kazyuka <rkazyuka@mail.ru>
2017-09-25 13:39:21 +0300

    Revert "Shema mongoose"

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 'use strict';
+
 var express = require('express');
-var bodyParser = require('body-parser')
 var app = express();
 
 var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/users');
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.listen(4000, function () {
-    console.log('Example app listening on port 4000!');
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
 
 require('./routes')(app);
