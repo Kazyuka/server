1c61b8069b4ccfecdaa7c8959e7dc1d2ad1fd59f
Kazyuka <rkazyuka@mail.ru>
2017-12-08 14:39:53 +0200

    added index.html

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 'use strict';
 var express = require('express');
 var bodyParser = require('body-parser')
+var path = require('path')
 var app = express();
 
 var mongoose = require('mongoose');
@@ -8,6 +9,10 @@ mongoose.connect('mongodb://localhost/users');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
+})
 
 app.listen(4000, function () {
     console.log('Example app listening on port 4000!');
